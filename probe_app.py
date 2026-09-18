ced76b555927aeafc9f8bfde423cd08445699f18
Manas-Dikshit <manasdikshit48@gmail.com>
2026-09-19 02:12:58 +0530

    feat: remove unused FashionAnalyzer and CLIPModel imports from probe_app.py and probe_tf.py

--- probe_app.py
+++
@@ -1,3 +0,0 @@
-from analyzer import FashionAnalyzer
-a = FashionAnalyzer()
-print('FashionAnalyzer loaded OK')
--- probe_tf.py
+++
@@ -1,6 +0,0 @@
-import traceback
-try:
-    from transformers import CLIPModel, CLIPProcessor
-    print('OK')
-except Exception:
-    traceback.print_exc()
